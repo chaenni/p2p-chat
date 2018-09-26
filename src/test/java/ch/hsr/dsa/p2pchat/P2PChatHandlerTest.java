726733eaebbbb71ea5e5f6a8f5bfcdc55a1a6d2e
chaenni <chaenni@hsr.ch>
2018-09-26 19:31:32 +0200

    converted test to actual unit test

--- src/test/java/ch/hsr/dsa/p2pchat/P2PChatHandlerTest.java
+++ src/test/java/ch/hsr/dsa/p2pchat/P2PChatHandlerTest.java
@@ -1,24 +1,26 @@
 package ch.hsr.dsa.p2pchat;
 
+import ch.hsr.dsa.p2pchat.model.ChatMessage;
 import ch.hsr.dsa.p2pchat.model.User;
+import io.reactivex.observers.TestObserver;
 import java.io.IOException;
+import org.junit.jupiter.api.Test;
 
 public class P2PChatHandlerTest {
 
-    public static void main(String[] args) throws IOException {
+    @Test
+    public void test_should_receive_message() throws IOException {
         var peter = P2PChatHandler.start( "Peter", 4000);
         var hans = P2PChatHandler.start(peter.getPeerAddress(), "Hans", 4001);
 
-        peter.chatMessages().subscribe(message -> {
-            System.out.println("message received");
-        });
-
-        hans.chatMessages().subscribe(message -> {
-            System.out.println("message received");
-        });
+        var testObserver = new TestObserver<ChatMessage>();
+        hans.chatMessages().subscribe(testObserver);
 
         peter.sendMessage(new User("Hans"), "Sali Hans");
-        hans.sendMessage(new User("Peter"), "Sali Peter");
+
+        testObserver
+            .awaitCount(1)
+            .assertValueAt(0, message -> message.getMessage().equals("Sali Hans"));
     }
 
 }
