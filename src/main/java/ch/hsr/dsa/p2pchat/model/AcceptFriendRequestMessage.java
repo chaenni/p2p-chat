8c30c916538b005c76a6dce4cd3e23978e349dff
chaenni <chaenni@hsr.ch>
2018-10-02 13:49:45 +0200

    accept chatMessage and reject chatMessage

---
+++ src/main/java/ch/hsr/dsa/p2pchat/model/AcceptFriendRequestMessage.java
@@ -0,0 +1,14 @@
+package ch.hsr.dsa.p2pchat.model;
+
+public class AcceptFriendRequestMessage implements Message {
+    private User fromUser;
+
+    public AcceptFriendRequestMessage(User fromUser) {
+        this.fromUser = fromUser;
+    }
+
+    public User getFromUser() {
+        return fromUser;
+    }
+
+}
---
+++ src/main/java/ch/hsr/dsa/p2pchat/model/ChatConfiguration.java
@@ -0,0 +1,47 @@
+package ch.hsr.dsa.p2pchat.model;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class ChatConfiguration {
+
+    private Set<User> friends;
+    private Set<User> openFriendRequestsFromMe;
+    private Set<User> openFriendRequestsToMe;
+
+    public ChatConfiguration(Set<User> friends, Set<User> openFriendRequestsFromMe, Set<User> openFriendRequestsToMe) {
+        this.friends = friends == null ? Collections.emptySet() : friends;
+        this.openFriendRequestsFromMe = openFriendRequestsFromMe
+            == null ? Collections.emptySet() : openFriendRequestsFromMe;
+        this.openFriendRequestsToMe = openFriendRequestsFromMe
+            == null ? Collections.emptySet() : openFriendRequestsFromMe;
+    }
+
+    public Set<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Set<User> friends) {
+        this.friends = friends;
+    }
+
+    public Set<User> getOpenFriendRequestsFromMe() {
+        return openFriendRequestsFromMe;
+    }
+
+    public void setOpenFriendRequestsFromMe(Set<User> openFriendRequestsFromMe) {
+        this.openFriendRequestsFromMe = openFriendRequestsFromMe;
+    }
+
+    public Set<User> getOpenFriendRequestsToMe() {
+        return openFriendRequestsToMe;
+    }
+
+    public void setOpenFriendRequestsToMe(Set<User> openFriendRequestsToMe) {
+        this.openFriendRequestsToMe = openFriendRequestsToMe;
+    }
+
+    public static ChatConfiguration empty() {
+        return new ChatConfiguration(null, null, null);
+    }
+}
