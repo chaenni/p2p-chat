62e1e6a66813943dd058a419c39daed55354a5b4
kesslerd <dev+github@kesslerd.ch>
2018-10-07 14:43:12 +0200

    tests

--- src/main/java/ch/hsr/dsa/p2pchat/ChatHandler.java
+++ src/main/java/ch/hsr/dsa/p2pchat/ChatHandler.java
@@ -9,6 +9,7 @@ import ch.hsr.dsa.p2pchat.model.User;
 import io.reactivex.Observable;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Optional;
 
 public interface ChatHandler {
     Observable<ChatMessage> chatMessages();
@@ -29,6 +30,7 @@ public interface ChatHandler {
     void createGroup(String name) throws IOException;
     void inviteToGroup(Group group, User toUser);
     void leaveGroup(Group group) throws IOException, ClassNotFoundException;
+    Optional<Group> getGroupInformation(String name) throws IOException, ClassNotFoundException;
 
     void acceptFriendRequest(User user);
     void rejectFriendRequest(User user);
